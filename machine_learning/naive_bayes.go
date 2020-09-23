053c08de2122032770680235cf001f39907b43ab
xenios <xenios91@protonmail.com>
2020-09-23 00:03:54 -0500

    started working on ml

---
+++ machine_learning/naive_bayes.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/navossoc/bayesian"
+)
+
+const (
+	Good bayesian.Class = "Good"
+	Bad  bayesian.Class = "Bad"
+	Taco bayesian.Class = "Taco"
+)
+
+func main() {
+	classifier := bayesian.NewClassifier(Good, Bad, Taco)
+	goodStuff := []string{"tall", "rich", "handsome", "girl"}
+	badStuff := []string{"poor", "smelly", "ugly"}
+	tacoStuff := []string{"cheap", "mexican", "yummy"}
+	classifier.Learn(goodStuff, Good)
+	classifier.Learn(badStuff, Bad)
+	classifier.Learn(tacoStuff, Taco)
+
+	probs, _, _ := classifier.LogScores(
+		[]string{"yummy", "mexico"},
+	)
+
+	var highest int = 0
+	for count, _ := range probs {
+		if probs[count] > probs[highest] {
+			highest = count
+		}
+	}
+	fmt.Printf("The most likely classifier is: %s", classifier.Classes[highest])
+
+}
