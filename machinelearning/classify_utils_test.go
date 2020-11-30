8fc719acfd8528ef36d0385b06fc2e2324c08eac
xenios <xenios91@protonmail.com>
2020-11-29 19:34:55 -0600

    added more unit test

--- machinelearning/classify_utils_test.go
+++ machinelearning/classify_utils_test.go
@@ -137,3 +137,81 @@ func Test_populateNGrams(t *testing.T) {
 		t.Errorf("populateNGrams create the wrong tokens list, got %v want %v", functionDetails.Tokens, expectedSlice)
 	}
 }
+
+func Test_setClassifierConfig(t *testing.T) {
+	type args struct {
+		nGrams int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "test1", args: args{nGrams: 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setClassifierConfig(tt.args.nGrams)
+		})
+	}
+
+	got := classifierConfig.NGrams
+	want := 2
+	if got != want {
+		t.Errorf("Classifier Config has incorrect N-Grams, got %v want %v", got, want)
+	}
+}
+
+func Test_populateReturnTypeMap(t *testing.T) {
+	classes := make(map[bayesian.Class]bin_utils.FunctionDetails)
+	function := new(bin_utils.FunctionDetails)
+	function.FunctionName = "testfunction"
+	function.ReturnType = "void"
+	classes[bayesian.Class(function.FunctionName)] = *function
+	type args struct {
+		classes *map[bayesian.Class]bin_utils.FunctionDetails
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "test1", args: args{classes: &classes}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			populateReturnTypeMap(tt.args.classes)
+		})
+	}
+	returnTypeMapLength := len(returnTypeMap)
+	if returnTypeMapLength != 1 {
+		t.Errorf("ReturnTypeMap is unexpected size, got %v want %v", returnTypeMapLength, 1)
+	}
+	returnTypeVoidSlice := returnTypeMap["void"]
+	obtainedFunction := returnTypeVoidSlice[0]
+	if !reflect.DeepEqual(obtainedFunction, *function) {
+		t.Errorf("ReturnTypeMap stored incorrect function with return type %v got %v want %v", function.ReturnType, obtainedFunction, function)
+	}
+}
+
+func Test_retrieveReturnTypeFromTokens(t *testing.T) {
+	function := new(bin_utils.FunctionDetails)
+	function.FunctionName = "testFunction"
+	function.Tokens = []string{"void", "testFunction", "something", "something", "darkside"}
+	want := "void"
+	type args struct {
+		function *bin_utils.FunctionDetails
+	}
+	tests := []struct {
+		name string
+		args args
+		want *string
+	}{
+		{name: "test1", args: args{function: function}, want: &want},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := retrieveReturnTypeFromTokens(tt.args.function); *got != *tt.want {
+				t.Errorf("retrieveReturnTypeFromTokens() = %v, want %v", *got, *tt.want)
+			}
+		})
+	}
+}
