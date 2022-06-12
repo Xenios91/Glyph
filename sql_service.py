fde6dda222ecaf4289c796fc9b71fdb0eb24d416
Corey <xenios91@protonmail.com>
2022-06-12 02:29:15 +0000

    added deletion from the table

--- sql_service.py
+++ sql_service.py
@@ -13,6 +13,7 @@ class SQLUtil():
 
                 sql = "INSERT INTO models (model_name, model) VALUES (?, ?)"
                 cur.execute(sql, (model_name, sqlite3.Binary(model)))
+                con.commit()
             except Exception as e:
                 print(e)
 
@@ -39,5 +40,16 @@ class SQLUtil():
                 sql = "SELECT * FROM MODELS WHERE model_name=?"
                 model = cur.execute(sql, (model_name,)).fetchone()
                 return model
+            except Exception as e:
+                print(e)
+
+    @staticmethod
+    def delete_model(model_name: str):
+        with sqlite3.connect('models.db') as con:
+            try:
+                cur = con.cursor()
+                sql = "DELETE FROM MODELS WHERE model_name=?"
+                cur.execute(sql, (model_name,))
+                con.commit()
             except Exception as e:
                 print(e)
