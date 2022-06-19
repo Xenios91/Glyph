61abcd6f54563dac875062e136cf3a48ffaab75e
Corey <xenios91@protonmail.com>
2022-06-19 18:15:02 +0000

    turned trainer into a singleton

--- task_management.py
+++ task_management.py
@@ -64,6 +64,15 @@ class TaskManager():
 
 
 class Trainer(TaskManager):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self) -> None:
+        super().__init__()
 
     @classmethod
     def start_training(cls, training_request: TrainingRequest):
