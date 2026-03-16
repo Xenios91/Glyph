525f93dc52607b1cbc7a4142ab0ef3d39f3cb4bc
Dr. Corey Hartman <35783364+Xenios91@users.noreply.github.com>
2026-03-16 01:58:00 +0000

    bug fixes

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-from app.routers import binaries_api, predictions_api
+from fastapi.staticfiles import StaticFiles
+from app.routers import binaries_api, predictions_api, views
 
-app = FastAPI()
-app.include_router(binaries_api.router)
-app.include_router(predictions_api.router)
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+    app.include_router(views.router)
+    app.include_router(binaries_api.router)
+    app.include_router(predictions_api.router)
+
+    return app
+
+
+app = create_app()
