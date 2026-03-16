ac4229f7a72eba0f3e78320c00126e952ad006d3
Dr. Corey Hartman <35783364+Xenios91@users.noreply.github.com>
2026-03-16 02:22:43 +0000

    more updates

---
+++ app/routers/task_api.py
@@ -0,0 +1,75 @@
+import logging
+from typing import Optional, Any
+
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+from app.persistance_util import FunctionPersistanceUtil, MLPersistanceUtil
+from app.request_handler import PredictionRequest, TrainingRequest
+from app.task_management import Predictor, Trainer
+
+router = APIRouter()
+
+
+class TaskRequest(BaseModel):
+    type: str
+    modelName: Optional[str] = None
+    uuid: Optional[str] = None
+    overwriteModel: Optional[bool] = False
+    data: Optional[Any] = None
+
+
+def train_model(request: TaskRequest) -> JSONResponse:
+    """
+    Creates a job to train an ml model on the tokens supplied
+    """
+    if request.modelName is None:
+        return JSONResponse(content={"error": "model name is invalid"}, status_code=400)
+
+    uuid = request.uuid or Trainer().get_uuid()
+
+    if not request.overwriteModel and MLPersistanceUtil.check_name(request.modelName):
+        return JSONResponse(content={"error": "model name already taken"}, status_code=400)
+
+    try:
+        training_request = TrainingRequest(uuid, request.modelName, request.model_dump())
+        Trainer().start_training(training_request)
+        FunctionPersistanceUtil.add_model_functions(training_request)
+
+        return JSONResponse(content={"uuid": training_request.uuid}, status_code=201)
+    except TypeError as type_error:
+        logging.error(type_error)
+        return JSONResponse(content={"error": "type error"}, status_code=400)
+
+
+def predict_tokens(request: TaskRequest) -> JSONResponse:
+    """
+    Creates a job to predict a function name based on the tokens supplied
+    """
+    if request.modelName is None:
+        return JSONResponse(content={"error": "model name is invalid"}, status_code=400)
+
+    uuid = request.uuid or Trainer().get_uuid()
+
+    try:
+        prediction_request = PredictionRequest(uuid, request.modelName, request.model_dump())
+        Predictor().start_prediction(prediction_request)
+
+        return JSONResponse(content={"uuid": prediction_request.uuid}, status_code=201)
+    except TypeError as type_error:
+        logging.error(type_error)
+        return JSONResponse(content={"error": "type error"}, status_code=400)
+
+
+@router.post("/task")
+def handle_task(request: TaskRequest) -> JSONResponse:
+    """
+    Handles a train/predict task
+    """
+    if request.type == "training":
+        return train_model(request)
+    elif request.type == "prediction":
+        return predict_tokens(request)
+    else:
+        return JSONResponse(content={"error": "Invalid request type"}, status_code=400)
