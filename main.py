3e2a84fd7fa95167d654b0cba18255cc64faf8e6
Dr. Corey Hartman <35783364+Xenios91@users.noreply.github.com>
2026-03-17 00:55:09 +0000

    bug fixes for fastapi changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import threading
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.config import GlyphConfig
-from app.routers import binaries_api, predictions_api, views
+from app.routers import binaries_api, models_api, other_api, predictions_api, views
 from app.services import TaskService
 from app.sql_service import SQLUtil
 
@@ -16,6 +16,8 @@ def create_app() -> FastAPI:
     app.include_router(views.router)
     app.include_router(binaries_api.router)
     app.include_router(predictions_api.router)
+    app.include_router(models_api.router)
+    app.include_router(other_api.router)
 
     return app
 
